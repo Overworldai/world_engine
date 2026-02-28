cb05b270c40657c66019c30911b6d75c5339bba1
anm-ol <anmolagrawal5704@gmail.com>
2026-02-28 05:33:56 +0000

    add websocket stream

--- examples/gen_sample.py
+++ examples/gen_sample.py
@@ -1,15 +1,14 @@
 import cv2
+from tqdm import tqdm
 from world_engine import WorldEngine
 
 
 def gen_vid():
     # engine = WorldEngine("OpenWorldLabs/CoDCtl-Causal-Flux-SelfForcing", device="cuda")
-    engine = WorldEngine("/mnt/data/laplace/models/combat_sfpp/step_640", model_config_overrides={"n_frames": 24000}, device="cuda")
+    engine = WorldEngine("/mnt/data/laplace/models/combat_sfpp/step_640", model_config_overrides={"n_frames": 640}, device="cuda")
     writer = None
-    for i in range(2400):
-        print(f"Generating frame {i}")
+    for i in tqdm(range(640)):
         frame = engine.gen_frame()
-        print(frame.shape, frame.dtype, frame.device)
         frame = frame.cpu().numpy()[:, :, ::-1]  # RGB -> BGR for OpenCV
         writer = writer or cv2.VideoWriter(
             "out.mp4",
