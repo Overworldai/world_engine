0f2960eea958e9529d182f38230c94fb7c0a38e3
Anmol Agarwal <anmolagrawal5704@gmail.com>
2026-03-14 04:44:40 +0530

    testing

--- examples/gen_sample.py
+++ examples/gen_sample.py
@@ -13,7 +13,7 @@ from world_engine import WorldEngine, CtrlInput
 
 
 # Create inference engine
-engine = WorldEngine(sys.argv[1], device="cuda")
+engine = WorldEngine(sys.argv[1], quant="int4_weights", device="cuda")
 
 
 # Set seed frame
