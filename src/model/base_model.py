514797e23f30965db22922483bdea605973b66c5
Anmol Agarwal <anmolagrawal5704@gmail.com>
2026-03-15 01:00:56 +0530

    yuh

--- src/model/base_model.py
+++ src/model/base_model.py
@@ -6,7 +6,7 @@ from safetensors.torch import load_file
 from torch import nn
 import torch
 
-from quantize import apply_qat
+from ..quantize import apply_qat
 
 class BaseModel(nn.Module):
     @classmethod
