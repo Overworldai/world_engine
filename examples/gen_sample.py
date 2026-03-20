e8cd112ddf5f83efe6058a8572fca9ab65e70be6
Andrew Lapp <andrew@github.rew.la>
2026-03-20 12:46:41 -0400

    improve example w/ four_frames var

--- examples/gen_sample.py
+++ examples/gen_sample.py
@@ -18,18 +18,6 @@ from world_engine import WorldEngine, CtrlInput
 engine = WorldEngine(sys.argv[1], device="cuda")
 
 
-# Set seed frame
-url = random.choice([
-    "https://gist.github.com/user-attachments/assets/d81c6d26-a838-4afe-9d13-fd67677043c3",
-    "https://gist.github.com/user-attachments/assets/b6d18c38-098e-43b0-8e61-66a16e5d8946",
-    "https://gist.github.com/user-attachments/assets/0734a8c1-3eb4-4ffe-8c37-5665c45ab559",
-    "https://gist.github.com/user-attachments/assets/f9c20d4d-7565-452d-8b02-42a85ea175ed",
-    "https://gist.github.com/user-attachments/assets/68c943a4-008a-4c25-948c-c81ab4c47d21",
-])
-frame = cv2.imdecode(np.frombuffer(urllib.request.urlopen(url).read(), np.uint8), cv2.IMREAD_COLOR)
-engine.append_frame(torch.from_numpy(np.repeat(frame[None], 4, axis=0)))
-
-
 # Define sequence of controller inputs applied
 controller_sequence = [
     # move mouse, jump, do nothing, trigger, do nothing, trigger+jump, do nothing
@@ -47,10 +35,22 @@ controller_sequence += (
 controller_sequence += [CtrlInput()] * 10
 
 
+# Set seed frame
+url = random.choice([
+    "https://gist.github.com/user-attachments/assets/d81c6d26-a838-4afe-9d13-fd67677043c3",
+    "https://gist.github.com/user-attachments/assets/b6d18c38-098e-43b0-8e61-66a16e5d8946",
+    "https://gist.github.com/user-attachments/assets/0734a8c1-3eb4-4ffe-8c37-5665c45ab559",
+    "https://gist.github.com/user-attachments/assets/f9c20d4d-7565-452d-8b02-42a85ea175ed",
+    "https://gist.github.com/user-attachments/assets/68c943a4-008a-4c25-948c-c81ab4c47d21",
+])
+seed_frame = cv2.imdecode(np.frombuffer(urllib.request.urlopen(url).read(), np.uint8), cv2.IMREAD_COLOR)
+seed_frame_x4 = torch.from_numpy(np.repeat(seed_frame[None], 4, axis=0))
+
+
 # Generate frames conditioned on controller inputs
 with iio.imopen("out.mp4", "w", plugin="pyav") as out:
-    four_frames = engine.gen_frame().cpu().numpy()  # int8 [4, H, W, 3]
-    out.write(four_frames, fps=60, codec="libx264")
+    engine.append_frame(seed_frame_x4)
+    out.write(seed_frame_x4, fps=60, codec="libx264")
     for ctrl in controller_sequence:
         four_frames = engine.gen_frame(ctrl=ctrl).cpu().numpy()
         out.write(four_frames)
