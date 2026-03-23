60c886486803d167cc381d1c3f96217cf340eeb0
Andrew Lapp <andrew@github.rew.la>
2026-03-23 14:46:56 -0400

    no compile prep inputs

--- src/world_engine.py
+++ src/world_engine.py
@@ -159,7 +159,6 @@ class WorldEngine:
 
         return self._ctx
 
-    @torch.compile
     def prep_inputs(self, x, ctrl=None):
         ctrl = ctrl if ctrl is not None else CtrlInput()
         self._ctx["button"].zero_()
